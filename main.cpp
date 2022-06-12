44b27ac0609057a88f6aae87d044054216d50df5
Ginormous712 <105841563+Ginormous712@users.noreply.github.com>
2022-06-12 11:00:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -74,35 +74,114 @@ namespace lab2b
 
 	};
 
+	
+#define MAX_LEVEL 6
+	const double p = 0.5; // probability(usually 1/2 or 1/4)
+
+	struct snode
+	{
+		int value;
+		snode** forw;
+		snode(int level, int& value)
+		{
+			forw = new snode * [level + 1];
+			memset(forw, 0, sizeof(snode*) * (level + 1));
+			this->value = value;
+		}
+		~snode()
+		{
+			delete[] forw;
+		}
+	};
+	
 	struct SkipList
 	{
-		struct Node
-		{
-			Node* next;
-			int data;
-		};
-
-		Node* first;
-		Node* last;
-		size_t size;
-
+		snode* header;
+		int value;
+		int level;
 		SkipList()
 		{
-			first = nullptr;
-			last = nullptr;
-			size = 0;
+			header = new snode(MAX_LEVEL, value);
+			level = 0;
 		}
-
-		void add_element(int element)
+		~SkipList()
 		{
-
+			delete header;
 		}
-		void find_element(int element)
+		bool contains_element(int& s_value)
 		{
-
+			snode* x = header;
+			for (int i = level;i >= 0;i--)
+			{
+				while (x->forw[i] != NULL && x->forw[i]->value < s_value)
+				{
+					x = x->forw[i];
+				}
+			}
+			x = x->forw[0];
+			return x != NULL && x->value == s_value;
 		}
-
+		void add_element(int& value)
+		{
+			snode* x = header;
+			snode* update[MAX_LEVEL + 1];
+			memset(update, 0, sizeof(snode*) * (MAX_LEVEL + 1));
+			for (int i = level;i >= 0;i--)
+			{
+				while (x->forw[i] != NULL && x->forw[i]->value < value)
+				{
+					x = x->forw[i];
+				}
+				update[i] = x;
+			}
+			x = x->forw[0];
+			if (x == NULL || x->value != value)
+			{
+				int lvl = random_level();
+				if (lvl > level)
+				{
+					for (int i = level + 1;i <= lvl;i++)
+					{
+						update[i] = header;
+					}
+					level = lvl;
+				}
+				x = new snode(lvl, value);
+				for (int i = 0;i <= lvl;i++)
+				{
+					x->forw[i] = update[i]->forw[i];
+					update[i]->forw[i] = x;
+				}
+			}
+		}
 	};
+
+
+	/*
+	 * Random Value Generator
+	 */
+	float frand()
+	{
+		return (float)rand() / RAND_MAX;
+	}
+
+	/*
+	 * Random Level Generator
+	 */
+	int random_level()
+	{
+		static bool first = true;
+		if (first)
+		{
+			srand((unsigned)time(NULL));
+			first = false;
+		}
+		int lvl = (int)(log(frand()) / log(1. - p));
+		return lvl < MAX_LEVEL ? lvl : MAX_LEVEL;
+	}
+
+
+
 
 	void test(bool num)
 	{
@@ -164,11 +243,24 @@ namespace lab2b
 						r = getch() - 48;
 						if (r == 1)
 						{
-
+							std::cout << "\t\t\t\t\tвведіть значеня елементу\n";
+							int el;
+							cin >> el;
+							a.add_element(el);
 						}
 						else if (r == 2)
 						{
-
+							std::cout << "\t\t\t\t\tвведіть значеня елементу\n";
+							int el;
+							cin >> el;
+							if (a.contains_element(el))
+							{
+								std::cout << "\t\t\t\t\tзнайедно\n";
+							}
+							else
+							{
+								std::cout << "\t\t\t\t\tне знайедно\n";
+							}
 						}
 						else
 						{
